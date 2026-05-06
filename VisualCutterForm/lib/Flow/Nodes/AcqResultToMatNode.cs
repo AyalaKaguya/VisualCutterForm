02faef9f62a14ae31fb8291c3ef1cb63d9af31ff
AyalaKaguya <ayalakaguya@outlook.com>
2026-05-06 15:19:20 +0800

    Add AcqResultToMatNode: converts AcquisitionResult to Mat + Width/Height outputs

---
+++ VisualCutterForm/lib/Flow/Nodes/AcqResultToMatNode.cs
@@ -0,0 +1,43 @@
+using System.Threading;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using VisualCutterForm.Lib.Flow.Data;
+
+namespace VisualCutterForm.Lib.Flow.Nodes
+{
+    [NodeCategory("转换", "取像结果转Mat")]
+    public class AcqResultToMatNode : FlowNode
+    {
+        [NodeInput("取像结果", Description = "AcquisitionResult")]
+        public AcquisitionResult Input { get; set; }
+
+        [NodeOutput("图像", Description = "Mat")]
+        public Mat Image { get; set; }
+
+        [NodeOutput("宽度", Description = "int")]
+        public int Width { get; set; }
+
+        [NodeOutput("高度", Description = "int")]
+        public int Height { get; set; }
+
+        public override Task ExecuteAsync(FlowContext context, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (Input != null && Input.Image != null && !Input.Image.IsDisposed && !Input.Image.Empty())
+            {
+                Image = Input.Image.Clone();
+                Width = Input.Width;
+                Height = Input.Height;
+            }
+            else
+            {
+                Image = null;
+                Width = 0;
+                Height = 0;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
