421b96cca503c4fd204dbf2ed0ffe3811a7e0168
AyalaKaguya <ayalakaguya@outlook.com>
2026-05-22 12:37:09 +0800

    RawBytesMonitor: add string receive mode, send section (HEX/string), append history [HH:mm:ss] ~> data, max 100 lines, Enter to send

--- VisualMaster.Communication/UI/RawBytesMonitorWindow.xaml.cs
+++ VisualMaster.Communication/UI/RawBytesMonitorWindow.xaml.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Linq;
+using System.Text;
 using System.Windows;
+using System.Windows.Input;
 using VisualMaster.Communication.Api;
 using VisualMaster.Communication.Core;
 
@@ -7,6 +10,8 @@ namespace VisualMaster.Communication.UI
 {
     public partial class RawBytesMonitorWindow : Window
     {
+        private const int MaxHistoryLines = 100;
+
         private readonly CommunicationManager _manager;
         private readonly string _deviceId;
         private readonly string _blockId;
@@ -25,11 +30,51 @@ namespace VisualMaster.Communication.UI
             if (e.DeviceId != _deviceId || e.BlockId != _blockId) return;
             Dispatcher.BeginInvoke(new Action(() =>
             {
+                string formatted;
                 if (ShortMode.IsChecked == true)
-                    ValueBox.Text = BitConverter.ToString(e.Data).Replace("-", " ");
+                    formatted = BitConverter.ToString(e.Data).Replace("-", " ");
+                else if (StringMode.IsChecked == true)
+                    formatted = Encoding.ASCII.GetString(e.Data);
                 else
-                    ValueBox.Text = CommunicationDataConverter.ToHex(e.Data);
+                    formatted = CommunicationDataConverter.ToHex(e.Data);
+
+                string timestamp = DateTime.Now.ToString("HH:mm:ss");
+                ValueBox.AppendText($"[{timestamp}] -> {formatted}\n");
+                PruneHistory();
+                ValueBox.ScrollToEnd();
             }));
+        }
+
+        private void PruneHistory()
+        {
+            var lines = ValueBox.Text.Split('\n');
+            if (lines.Length <= MaxHistoryLines) return;
+            ValueBox.Text = string.Join("\n", lines.Skip(lines.Length - MaxHistoryLines));
+        }
+
+        private async void SendData()
+        {
+            string text = SendBox.Text;
+            if (string.IsNullOrEmpty(text)) return;
+
+            byte[] data;
+            if (SendStringMode.IsChecked == true)
+                data = Encoding.ASCII.GetBytes(text);
+            else
+                data = CommunicationDataConverter.FromHex(text);
+
+            await _manager.WriteBlockAsync(_deviceId, _blockId, data);
+        }
+
+        private void SendBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                SendData();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendData();
         }
 
         protected override void OnClosed(EventArgs e)
